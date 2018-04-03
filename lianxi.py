ccdc6a0fd97436738f2866aa0c67bc3896720e86
542450010@qq.com <54245001@qq.com>
2018-04-03 15:11:17 +0800

    'jmlianxi'

--- lianxi.py
+++ lianxi.py
@@ -1,9 +1,4 @@
 class Foo(object):
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 44bf33a0a926e19b4710307f50dedce347816feb
     def __add__(self, other):
         return 987
     def __mul__(self, other):
@@ -12,15 +7,8 @@ class Foo(object):
         return 654
 
 
-
 a = Foo()
 b = Foo()
-<<<<<<< HEAD
-print(a*b)
-print(a+b)
-print(a-b)
-=======
 print(a*b) # 123
 print(a+b) # 987
 print(a-b) # 654
->>>>>>> 44bf33a0a926e19b4710307f50dedce347816feb
