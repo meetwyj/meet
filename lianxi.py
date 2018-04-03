4b7f9c2cabe867a7fe41f5639fb06e518ca964a0
yan <yyan@email.com>
2018-04-03 15:23:04 +0800

    yxy03

--- lianxi.py
+++ lianxi.py
@@ -1,9 +1,5 @@
 class Foo(object):
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 44bf33a0a926e19b4710307f50dedce347816feb
     def __add__(self, other):
         return 987
     def __mul__(self, other):
@@ -17,3 +13,4 @@ print('Hello World')
 print(a*b) # 123
 print(a+b) # 987
 print(a-b) # 654
+print('yxy03')
